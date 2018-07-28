350983282cde2520b31b4038166945b9b4de7f4f
Jack van Driel <jack.driel.12@ucl.ac.uk>
2018-07-27 22:11:03 -0700

    Addded strain and fabric open

--- fabric_open.py
+++ fabric_open.py
@@ -1,1 +1,32 @@
-# This File is designed to open outputfiles from VPSC
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import pandas as pd
+import math as math
+from math import sin
+from decimal import Decimal
+
+#print np.version.version
+
+# Open Strain Data
+a=open(sys.argv[1])
+strain_data=[]
+for line in a:
+	strain_line = a.readline().split()
+	strain_data.append(strain_line)
+
+
+# Open Fabric Data
+fabric_data=[]
+b=open(sys.argv[2])
+b.readline()
+b.readline()
+b.readline()
+b.readline()
+for line in b:
+	fabric_line = b.readline().split()
+	fabric_data.append(fabric_line)
+
+
+
