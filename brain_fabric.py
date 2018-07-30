7a924659841efbbdd4bfaff83d3f47cfba8c3ed9
Jack van Driel <jack.driel.12@ucl.ac.uk>
2018-07-29 18:59:00 -0700

    Realised that brain fabric only requires 2 line of code

--- brain_fabric.py
+++ brain_fabric.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
 
+import sys
+import numpy as np
+import pandas as pd
+import math as math
+from math import sin
+from decimal import Decimal
+
+#print np.version.version
+
+# Open Strain Data
+
+strain_data = np.loadtxt(sys.argv[1], delimiter=' ')
+
+# Open Fabric Data
+
+fabric_data = np.genfromtxt(sys.argv[2], delimiter=(8, 8,8,6,6,6,6,6,3,5,3, 3), skip_header=(8))
